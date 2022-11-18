3a69241d8049454769aa8ff81e48b11fb7896582
Gabriel Guimel <gabriel.guimelrn@gmail.com>
2022-11-17 22:34:28 -0300

    criando os componentes de card e do botão

--- src/components/Application/Index.jsx
+++ src/components/Application/Index.jsx
@@ -1,5 +1,24 @@
+import { CardCharacter } from "../CardCharacter/Index";
+import { ContainerApp, ContentCharacters, HeaderApp } from "./styles";
+
 export function Application() {
     return (
-        <h1>API</h1>
+        <ContainerApp>
+            <HeaderApp>
+                <h1>Rick and Morty</h1>
+                <span>Nª de personagens: 826</span>
+            </HeaderApp>
+            <ContentCharacters>
+                <div>
+                    <CardCharacter
+                        image="https://rickandmortyapi.com/api/character/avatar/441.jpeg"
+                        name="Rick Sanchez"
+                        genre="Male"
+                        specie="Human"
+                    />
+                </div>
+                <button>Carregar mais</button>
+            </ContentCharacters>
+        </ContainerApp>
     )
 }
--- src/components/Application/styles.js
+++ src/components/Application/styles.js
@@ -6,3 +6,48 @@ export const ContainerApp = styled.div`
     margin: 0 auto;
     padding: 4rem 15px;
 `;
+
+export const HeaderApp = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+
+    padding-bottom: 1.6rem;
+    margin-bottom: 1.6rem;
+    h1 {
+        font-size: 3.2rem;
+    }
+    span {
+        opacity: 0.6;
+    }
+`;
+
+export const ContentCharacters = styled.div`
+    & > div {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-gap: 1.6rem;
+    }
+
+    button {
+        display: block;
+        line-height: 6.5rem;
+        background-color: #5b1fa6;
+        border-radius: 6px;
+        width: 30rem;
+
+        margin: 0 auto;
+        margin-top: 4.8rem;
+
+        cursor: pointer;
+
+        border: none;
+        font-size: 1.8rem;
+
+        transition:  filter .4s ease;
+        &:hover {
+            filter: brightness(0.8);
+        }
+    }
+`;
